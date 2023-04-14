7712acec97e430949e3f932584f4389fca5c2cc4
Lalamoon21 <130730435+Lalamoon21@users.noreply.github.com>
2023-04-14 00:51:05 -0600

    Add files via upload

---
+++ resources.php
@@ -0,0 +1,143 @@
+
+<table width="50" height="191" align="center">
+  <tbody>
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+  </tbody>
+</table>
+
+
+
+<table width="372"  align="center">
+  <tbody>
+    <tr>
+      <td width="9">&nbsp;</td>
+      <td colspan="8"><h2 align="center">Photo Sources</h2></td>
+      <td width="8">&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td width="236">&nbsp;</td>
+      <td width="9">&nbsp;</td>
+      <td width="9">&nbsp;</td>
+      <td width="9">&nbsp;</td>
+      <td width="9">&nbsp;</td>
+      <td width="9">&nbsp;</td>
+      <td width="9">&nbsp;</td>
+      <td width="9">&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td><ul>
+        <li><a href="https://www.freepik.com/photos/portrait">Portrait pictures</a></li>
+        <li><a href="https://www.freepik.com/search?format=search&query=Pets">Pets pictures</a></li>
+        <li><a href="https://www.freepik.com/search?format=search&query=Projects">Projects pictures</a></li>
+        <li><a href="https://www.freepik.com/search?format=search&page=2&query=hobbies">Hobbies pictures</a></li>
+      </ul></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td colspan="8"><h2 align="center">Resources</h2></td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>      <ul>
+        <li><a href="https://getbootstrap.com/">Bootstrap v4.3.1</a></li>
+        <li><a href="https://tympanus.net/Development/HoverEffectIdeas/">HoverEffectIdeas</a></li>
+        <li><a href="https://fontawesome.com">Font Awesome</a></li>
+        <li><a href="http://pixelcog.github.io/parallax.js/">Parallax.js</a></li>
+        <li><a href="http://isotope.metafizzy.co">Isotope</a></li>
+        <li><a href="http://dimsemenov.com/plugins/magnific-popup/">Magnific Popup</a></li>
+        <li><a href="https://codepen.io/TimRizzo/details/ervrRq">Tim Rizzo's Codepen</a></li>
+        <li><a href="http://kenwheeler.github.io">Ken Wheeler's Website</a></li>
+        <li><a href="http://kenwheeler.github.io/slick">Slick</a></li>
+        <li><a href="http://github.com/kenwheeler/slick">Slick on Github</a></li>
+        <li><a href="http://github.com/kenwheeler/slick/issues">Slick Issues on Github</a></li>
+</ul></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+  </tbody>
+</table>
+
+
+
+      
+
